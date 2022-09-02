fd787a2404df17d380abc8987a57e691c4e59e7f
imhyeonji <imhyeonji0118@gmail.com>
2022-09-02 22:01:03 +0900

    Add wallet connection

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,37 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { WagmiConfig, createClient, chain, configureChains } from "wagmi";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+
+const { chains, provider } = configureChains(
+  //   [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum],
+  [chain.goerli],
+  [
+    alchemyProvider({ apiKey: "dCQDoMQTsnzhCdUscKkDf-AFd15FUr3Z" }),
+    publicProvider(),
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "My RainbowKit App",
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains} modalSize="compact">
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
 }
 
-export default MyApp
+export default MyApp;
